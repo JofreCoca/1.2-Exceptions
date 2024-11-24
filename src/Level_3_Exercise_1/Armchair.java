569eaa9027ec9e72d3c1a574df7eeb91eac93b1d
JofreCoca <jofrecoca@gmail.com>
2024-11-24 09:12:00 +0100

    Armchair finish in level 3

--- src/Level_3_Exercise_1/Armchair.java
+++ src/Level_3_Exercise_1/Armchair.java
@@ -1,6 +1,41 @@
 package Level_3_Exercise_1;
 
 public class Armchair {
-    public Armchair() {
+    int rowNumber;
+    int numberOfSeats;
+    String personWhoReservesTheSeat;
+    public Armchair(int rowNumber,int numberOfSeats,String personWhoReservesTheSeat) {
+        this.rowNumber=rowNumber;
+        this.numberOfSeats=numberOfSeats;
+        this.personWhoReservesTheSeat=personWhoReservesTheSeat;
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public String getPersonWhoReservesTheSeat() {
+        return personWhoReservesTheSeat;
+    }
+
+    public boolean equals(Armchair armchair){
+        boolean result=false;
+        if (armchair.getRowNumber()==this.rowNumber && armchair.getNumberOfSeats()==this.numberOfSeats){
+            result=true;
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Armchair{" +
+                "rowNumber=" + rowNumber +
+                ", numberOfSeats=" + numberOfSeats +
+                ", personWhoReservesTheSeat='" + personWhoReservesTheSeat + '\'' +
+                '}';
     }
 }
